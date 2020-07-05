038ab43a04f9c970a7c865762e34c9829b156b38
unknown <linnieldw@gmail.com>
2020-07-05 20:12:11 +0200

    first commit

---
+++ Source/Hotseat.cs
@@ -0,0 +1,63 @@
+﻿using Verse;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace Hotseat
+{
+    public class HotseatSettings : ModSettings
+    {
+        /// <summary>
+        /// The three settings our mod has.
+        /// </summary>
+        public bool exampleBool;
+        public float exampleFloat = 200f;
+
+        /// <summary>
+        /// The part that writes our settings to file. Note that saving is by ref.
+        /// </summary>
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref exampleBool, "exampleBool");
+            Scribe_Values.Look(ref exampleFloat, "exampleFloat", 200f);
+                
+            base.ExposeData();
+        }
+    }
+    public class Hotseat // : Mod
+    {
+
+        HotseatSettings settings;
+
+        public Hotseat(ModContentPack content) // : base(content)
+        {
+            // this.settings = GetSettings<HotseatSettings>();
+
+            var harmony = new Harmony("com.arquebus.rimworld.mod.hotseat");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Harmony.DEBUG = true;
+
+            FileLog.Log("Hotseat harmony log");
+            Log.Message("Hotseat Loaded");
+        }
+
+        /*
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.CheckboxLabeled("exampleBoolExplanation", ref settings.exampleBool, "exampleBoolToolTip");
+            listingStandard.Label("exampleFloatExplanation");
+            settings.exampleFloat = listingStandard.Slider(settings.exampleFloat, 100f, 300f);
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "HotseatModSettings".Translate();
+        }
+        */
+    }
+}
---
+++ Source/PatchHStoryteller.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace Hotseat
+{
+    [HarmonyPatch(typeof(IncidentWorker))]
+    [HarmonyPatch("TryExecute")]
+    public static class PatchHStoryteller
+    {
+        static void Postfix()
+        {
+            Log.Message("tryfire postfix");
+            Log.Message(Current.Game.storyteller.def.defName);
+            /*Log.Message(Find.Storyteller.storytellerComps.ToString());*/
+
+            changeStoryTeller();
+
+            Log.Message("Storyteller changed.");
+            Log.Message(Current.Game.storyteller.def.defName);
+        }
+
+        private static void changeStoryTeller() {
+            if (Rand.RangeInclusive(0, 100) < 5){
+                chooseStoryTeller();
+            }
+            else { 
+                Log.Message("Storyteller remains... for now"); 
+            }
+            //TODO: This method doesn't work.
+        }
+
+        
+        private static void chooseStoryTeller() {
+            IEnumerable<StorytellerDef> storytellers = DefDatabase<StorytellerDef>.AllDefs;
+
+            StorytellerDef newStorytellerDef = storytellers.RandomElement();
+            Log.Message("Storyteller chosen is: "+newStorytellerDef.defName);
+            if (!newStorytellerDef.listVisible || newStorytellerDef == Current.Game.storyteller.def)
+            {
+                Log.Warning("new storyteller not suitable. Rechoosing.");
+                chooseStoryTeller();
+            }
+            else {
+                Current.Game.storyteller.def = newStorytellerDef;
+                Current.Game.storyteller.Notify_DefChanged();
+            }
+        }
+    }
+}
