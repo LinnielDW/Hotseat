68febd1d6647e930f1f49c1df62add15be78349a
Linniel Driver-Williams <linnieldw@googlemail.com>
2020-07-20 21:23:26 +0200

    Implemented HotseatGameComponent and storyteller change on tick modulo

---
+++ Source/HotseatGameComponent.cs
@@ -0,0 +1,29 @@
+﻿using Verse;
+
+namespace Hotseat
+{
+    class HotseatGameComponent : GameComponent
+    {
+        public HotseatGameComponent(Game game)
+        {
+        }
+
+        public override void GameComponentTick()
+        {
+            base.GameComponentTick();
+
+            int currentTick = Find.TickManager.TicksGame;
+            if (currentTick % 900000 == 0) //Quadrum
+            {
+                if (currentTick % 3600000 == 0) //Year
+                {
+                    Log.Message("Year happened. Check if storyteller changed.");
+                    HotseatUtils.TryChangeStoryTeller(HotseatSettings.changeOnYearChance);
+                    return;
+                }
+                Log.Message("Quadrum happened. Check if storyteller changed.");
+                HotseatUtils.TryChangeStoryTeller(HotseatSettings.changeOnQuadrumChance);
+            }
+        }
+    }
+}
